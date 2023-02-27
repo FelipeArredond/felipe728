e78d10f938ec1a3ad29817aaa8e80ab019ca8920
FelipeArredond <pipechiqui77@gamil.com>
2023-02-27 10:10:34 -0500

    changes

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -4,9 +4,10 @@ import "./css/video.css"
 export default function Video(){
     return(//tryyin to vercel detect commit
         <div className="video-container">
-            <video playsInline loop muted autoPlay>
+            {/* <video playsInline loop muted autoPlay>
                 <source src="../../assets/preVi2.mp4" type="video/mp4"/>
-            </video>
+            </video> */}
+            <img src="../../assets/mainImage.jpg" alt="" />
         </div>
     )
 }
