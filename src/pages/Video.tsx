230c479fd22ce841591c8b5d1624f1975322ca3e
FelipeArredond <pipechiqui77@gamil.com>
2023-02-27 21:13:09 -0500

    Video from server

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -5,7 +5,7 @@ export default function Video(){
     return(//tryyin to vercel detect commit
         <div className="video-container">
             <video playsInline loop muted autoPlay>
-                <source src="../../assets/preVi2.mp4" type="video/mp4"/>
+                <source src="https://firebasestorage.googleapis.com/v0/b/felipe728-c4a03.appspot.com/o/preVi2.mp4?alt=media&token=66155df9-5e81-4503-9820-932bce59cc02" type="video/mp4"/>
             </video>
         </div>
     )
